1ebc3ad19ac479a20f9a30bc25e8b00286ebdb02
vthinkxie <yadong.xyd@alibaba-inc.com>
2020-07-14 11:35:07 +0800

    feat: demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import {NzDropDownModule} from 'ng-zorro-antd/dropdown';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NzDropDownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
