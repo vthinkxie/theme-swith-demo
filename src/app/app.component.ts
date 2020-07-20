9905a9d2aa3aa023e7e92a1c1516544a65a6c599
vthinkxie <yadong.xyd@alibaba-inc.com>
2020-07-20 18:26:31 +0800

    feat: add switch demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'neo';
+export class AppComponent implements OnInit {
+  loading = true;
+  theme = localStorage.getItem('theme') === 'default' ? 'default' : 'dark';
+  loadCss(href: string) {
+    const style = document.createElement('link');
+    style.type = 'text/css';
+    style.href = href;
+    style.rel = 'stylesheet';
+    document.body.append(style);
+  }
+  loadScript(src: string) {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = src;
+      script.onload = resolve;
+      script.onerror = reject;
+      document.body!.appendChild(script);
+    });
+  }
+
+  toggleTheme() {
+    if (this.theme === 'dark') {
+      this.theme = 'default';
+    } else {
+      this.theme = 'dark';
+    }
+    localStorage.setItem('theme', this.theme);
+    document.documentElement.className = this.theme;
+    this.loadThemeAssets();
+  }
+
+  loadThemeAssets() {
+    if (environment.production) {
+      this.loadCss(`./${this.theme}.css`);
+      setTimeout(() => {
+        this.loading = false;
+      }, 1000);
+    } else {
+      this.loadScript(`./${this.theme}.js`).then(() => {
+        this.loading = false;
+      });
+    }
+  }
+  ngOnInit() {
+    this.loadThemeAssets();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {NzDropDownModule} from 'ng-zorro-antd/dropdown';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    NzDropDownModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, NzLayoutModule, NzButtonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
